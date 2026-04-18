86a3cab7b810b3166f98d5e43a99703a8fe172fb
Kirill <kirillmikulich8@gmail.com>
2026-04-18 14:27:11 +0300

    исправлена кнопка войти

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,14 +4,26 @@ function Header({isVisible, setIsVisible}){
         <header className="flex justify-between items-center p-4">
             <h1>Интернет-магазин</h1>
             
-            <button 
-                onClick={() => setIsVisible(!isVisible)}
-                className="!bg-black !text-white px-4 py-2 rounded"
-            >
-                {isVisible ? "выход" : "вход"}
-            </button>
+            <div>
+                {isVisible === true ? (
+                    <button 
+                        onClick={() => setIsVisible(false)}
+                        className="!bg-black !text-white px-4 py-2 rounded"
+                    >
+                        Выйти
+                    </button>
+                ) : (
+                    <button 
+                        onClick={() => setIsVisible(true)}
+                        className="!bg-black !text-white px-4 py-2 rounded"
+                    >
+                        Войти
+                    </button>
+                )}
+            </div>
         </header>
     )
 }
 
 export default Header
+
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,12 +1,17 @@
+import { useState } from "react"
 import Header from "../Header"
 import ProductList from "../components/blocks/ProductList"
 
 function Main() {
+    const [isVisible, setIsVisible] = useState(false)
+
     return(
         <div>
-            <Header/>
+            <Header isVisible={isVisible} setIsVisible={setIsVisible} />
             <h2>Главная страница</h2>
             <ProductList />
+            
+           
         </div>
     )
 }
